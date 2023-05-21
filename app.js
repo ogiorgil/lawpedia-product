c857e9f35d6d02059bc7cfa4e5dea923247f0996
KevinRazaqa <kevin.razaqa@ui.ad.id>
2023-05-21 23:14:04 +0700

    adding auth

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const axios = require('axios');
 const app = express();
 
 app.use(express.json());
@@ -22,8 +22,40 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 
+// Middleware function for token validation and role-based authorization
+const validateToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+
+    // Make a request to your Python server for token validation
+    const response = await axios.post('http://127.0.0.1:8000/auth/user', null, {
+      headers: {
+        Authorization: token,
+      },
+    });
+
+    if (response.data && response.data.username) {
+      const role = response.data.role;
+      if (role === 2) {
+        // User is authorized, proceed to the next middleware or route handler
+        req.username = response.data.username;
+        next();
+      } else {
+        // Unauthorized user
+        res.status(403).json({ error: 'Unauthorized' });
+      }
+    } else {
+      // Unauthorized user
+      res.status(403).json({ error: 'Unauthorized' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
 // Create a new product
-app.post('/products/create', async (req, res) => {
+// Create a product
+app.post('/products/create', validateToken, async (req, res) => {
   try {
     const { id, username, name, price, stock, description, category } = req.body;
 
@@ -45,7 +77,7 @@ app.post('/products/create', async (req, res) => {
 });
 
 // Update a product
-app.put('/products/update/:id', async (req, res) => {
+app.put('/products/update/:id', validateToken, async (req, res) => {
   try {
     const { username, name, price, stock, description, category } = req.body;
     const productId = req.params.id;
@@ -91,13 +123,15 @@ app.get('/products/:id', async (req, res) => {
 });
 
 // Delete a product
-app.delete('/products/delete/:id', async (req, res) => {
+app.delete('/products/delete/:id', validateToken, async (req, res) => {
   try {
     const productId = req.params.id;
     const product = await Product.findOneAndDelete({ id: productId });
+
     if (!product) {
       return res.status(404).json({ error: 'Product not found' });
     }
+
     res.status(204).end();
   } catch (error) {
     res.status(404).json({ error: error.message });
